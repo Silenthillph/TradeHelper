bb309c84d6db2e56c3356d24e17a34a123286e93
YaroslavHrynchuk <hrynchukyaroslav@gmail.com>
2018-06-22 21:53:40 +0300

    added ng-bootstrap

--- TradeHelper.WebApp/angular-app/src/app/app.module.ts
+++ TradeHelper.WebApp/angular-app/src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './startup/app.component';
 import { TradeService } from './service/trade.service';
 import { AppRoutingModule } from './startup/app-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TradesComponent } from './components/trades/trades.component'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent, 
+    TradesComponent    
   ],
   imports: [
     BrowserModule,
-    //HttpModule,
-    // HttpClientModule,
+    HttpModule,
+    HttpClientModule,
     AppRoutingModule,   
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- TradeHelper.WebApp/angular-app/src/app/components/trades/trades.component.ts
+++ TradeHelper.WebApp/angular-app/src/app/components/trades/trades.component.ts
@@ -1,71 +1,50 @@
-﻿// import { Component, Input, OnInit } from '@angular/core';
-// import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-// import { TradeService } from '../../service/trade.service';
-// import { ITradeInfo } from "../../model/tradeInfo";
-// import { Enums } from "../../common/enums";
-// import { Utils } from "../../common/utils"
+﻿import { Component, OnInit } from '@angular/core';
+import { NgbModule, NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { TradeService } from '../../service/trade.service';
+import { ITradeInfo } from "../../models/tradeInfo";
+import { Enums } from "../../common/enums";
+import { Utils } from "../../common/utils"
 
-// @Component({
-//     templateUrl: "app/components/trades/trades.component.html"
-// })
+@Component({
+    templateUrl: './trades.component.html',
+    selector: 'app-trades',
+})
 
-// export class TradesComponent implements OnInit {
+export class TradesComponent implements OnInit {
+  
+    constructor(private _tradeService: TradeService, private _modalService: NgbModal) {
 
-//     modalTitle: string;
-//     modalBtnTitle: string;
-//     crudActionType: Enums.CrudOperation;
-//     trades: Array<ITradeInfo>;
-    
-//     constructor(private _tradeService: TradeService, private modalService: NgbModal) { }
+    }
 
-//     ngOnInit(): void {
-//        this.load();
-//     }
+    modalTitle: string;
+    modalBtnTitle: string;
+    crudActionType: Enums.CrudOperation;
+    trades: Array<ITradeInfo>;
 
-//     load(): void {   
-//         let $this = this;
-//         this._tradeService.getAllTrades()
-//             .then(trades => {
-//                 $this.trades = trades.json() || {};
-//             },
-//             error => { console.log(error); });
-//     }
+    ngOnInit() {
+       this.load();
+    }
 
-//     calculateProfit(trade: ITradeInfo): number {
-//         let result: number = 0;
-//         if (trade.Status == Enums.PositionStatus.Closed) {
-//             result = trade.Type == Enums.PositionType.Long
-//                 ? (trade.Amount * (trade.CellPrice - trade.BuyPrice))
-//                 : (trade.Amount * (trade.BuyPrice - trade.CellPrice));
-//         }
-//         return result;
-//     }
+    load(): void {   
+        let $this = this;
+        this._tradeService.getAllTrades()
+            .then(trades => {
+                $this.trades = trades.json() || {};
+            },
+            error => { console.log(error); });
+    }
 
-//     addTrade(content: any): void {
-//         const modalRef = this.modalService.open(NgbdModalContent);
-//         modalRef.componentInstance.name = 'World';
-//     }
-// }
+    calculateProfit(trade: ITradeInfo): number {
+        let result: number = 0;
+        if (trade.Status == Enums.PositionStatus.Closed) {
+            result = trade.Type == Enums.PositionType.Long
+                ? (trade.Amount * (trade.CellPrice - trade.BuyPrice))
+                : (trade.Amount * (trade.BuyPrice - trade.CellPrice));
+        }
+        return result;
+    }
 
-// @Component({
-//     selector: 'ngbd-modal-content',
-//     template: `
-//     <div class="modal-header">
-//       <h4 class="modal-title">Hi there!</h4>
-//       <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
-//         <span aria-hidden="true">&times;</span>
-//       </button>
-//     </div>
-//     <div class="modal-body">
-//       <p>Hello, {{name}}!</p>
-//     </div>
-//     <div class="modal-footer">
-//       <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
-//     </div>
-//   `
-// })
-// export class NgbdModalContent {
-//     @Input("name") name: any;
-
-//     constructor(public activeModal: NgbActiveModal) { }
-// }
+    addTrade(modalSelector: any): void {
+        const modalRef = this._modalService.open(modalSelector);
+    }
+}
